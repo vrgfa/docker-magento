21e4b26731de0473e49deaee5d4fa6ca67c6173e
vf <varga.ferenc.andras@gmail.com>
2018-01-01 16:46:46 +0100

    Add www-data user + group update based on incumbent user (snippet from magento 2 version)

--- 7.1/apache/entrypoint.sh
+++ 7.1/apache/entrypoint.sh
@@ -3,6 +3,33 @@
 [ "$DEBUG" = "true" ] && set -x
 
 
+# If asked, we'll ensure that the www-data is set to the same uid/gid as the
+# mounted volume.  This works around permission issues with virtualbox shared
+# folders.
+if [[ "$UPDATE_UID_GID" = "true" ]]; then
+    echo "Updating www-data uid and gid"
+
+    DOCKER_UID=`stat -c "%u" /var/www/html`
+    DOCKER_GID=`stat -c "%g" /var/www/html`
+
+    INCUMBENT_USER=`getent passwd $DOCKER_UID | cut -d: -f1`
+    INCUMBENT_GROUP=`getent group $DOCKER_GID | cut -d: -f1`
+
+    echo "Docker: uid = $DOCKER_UID, gid = $DOCKER_GID"
+    echo "Incumbent: user = $INCUMBENT_USER, group = $INCUMBENT_GROUP"
+
+    # Once we've established the ids and incumbent ids then we need to free them
+    # up (if necessary) and then make the change to www-data.
+
+    [ ! -z "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
+    usermod -u $DOCKER_UID www-data
+
+    [ ! -z "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
+    groupmod -g $DOCKER_GID www-data
+fi
+
+
+
 # Configure Sendmail if required
 if [ "$ENABLE_SENDMAIL" == "true" ]; then
     /etc/init.d/sendmail start
